12a31c1425171644161f6b20cc7a32848e34ce84
mustaphachqoubi <mustaphachqoubi88@gmail.com>
2023-04-08 17:03:34 +0000

    i just want to test something with git

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Home } from "./pages";
 import { Navbar, Leftbar } from "./components";
 
 function App() {
-  return (
+	return (
     <div className="flex justify-center ">
       <div className="fixed top-0 bottom-0 left-0 right-[30%] sm:right-[50%] md:relative md:w-80">
       <div className="fixed top-0 bottom-0 w-60">
